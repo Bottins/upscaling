39bed85d0d279fddedc5e398efc3aaf1a92ae86f
SalvAndrea <an.salvatore@outlook.it>
2026-04-27 12:48:46 +0200

    Update __init__.py
    Added ZG_loss

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,2 +1,2 @@
 from .registry import build_losses, LOSS_REGISTRY  # noqa: F401
-from . import data_loss, pde_loss, bc_loss, sharpness_loss, learned_loss, hessian_loss  # noqa: F401
+from . import data_loss, pde_loss, bc_loss, sharpness_loss, learned_loss, hessian_loss, ZG_loss  # noqa: F401
